1bd882055f8bbba50e63d99265dace1f59371563
01218279393 <478lemontree@gmail.com>
2026-03-01 21:57:23 +0900

    coding-test

---
+++ 1. 괄호 회전하기.py
@@ -0,0 +1,30 @@
+def solution(s):
+    answer = 0
+    n = len(s)
+
+    for i in range(n):
+        rotated = s[i:] + s[:i]
+
+        stack = []
+        valid = True
+
+        for ch in rotated:
+            if ch in "([{":
+                stack.append(ch)
+            else:
+                if not stack:
+                    valid = False
+                    break
+
+                last = stack.pop()
+
+                if (ch == ')' and last != '(') or \
+                   (ch == ']' and last != '[') or \
+                   (ch == '}' and last != '{'):
+                    valid = False
+                    break
+
+        if valid and not stack:
+            answer += 1
+
+    return answer
