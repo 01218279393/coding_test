e992735f013714276fb75ce974d44f76c628a785
01218279393 <478lemontree@gmail.com>
2026-04-03 16:49:03 +0900

    coding-test

--- JS/5. 배열 만들기2.js
+++ JS/5. 배열 만들기2.js
@@ -2,7 +2,6 @@ function solution(l, r) {
     const answer = [];
 
     for (let i = l; i <= r; i++) {
-        // 숫자를 문자열로 변환한 뒤, 배열로 만들어 모든 요소가 '0' 또는 '5'인지 확인
         const isEveryZeroOrFive = [...String(i)].every(char => char === '0' || char === '5');
 
         if (isEveryZeroOrFive) {
@@ -10,6 +9,5 @@ function solution(l, r) {
         }
     }
 
-    // 배열이 비어있으면 [-1], 값이 있으면 오름차순 배열 반환
     return answer.length === 0 ? [-1] : answer;
 }
