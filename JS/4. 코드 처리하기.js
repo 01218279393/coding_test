122db75d8ebe9b3182e84d3697ef43062be08d51
01218279393 <478lemontree@gmail.com>
2026-04-07 21:39:23 +0900

    coding-test

--- JS/4. 코드 처리하기.js
+++ JS/4. 코드 처리하기.js
@@ -6,10 +6,8 @@ function solution(code) {
         const char = code[idx];
 
         if (char === "1") {
-            // mode 전환 (0이면 1로, 1이면 0으로)
             mode = mode === 0 ? 1 : 0;
         } else {
-            // mode가 0일 때 짝수 인덱스 체크, mode가 1일 때 홀수 인덱스 체크
             if (mode === 0 && idx % 2 === 0) {
                 ret += char;
             } else if (mode === 1 && idx % 2 !== 0) {
@@ -18,6 +16,5 @@ function solution(code) {
         }
     }
 
-    // 결과가 빈 문자열이면 "EMPTY", 아니면 ret 반환
     return ret === '' ? "EMPTY" : ret;
 }
